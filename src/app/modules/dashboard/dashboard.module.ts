2e8c14eded5b14c991b2175d135a9f56a3086153
Ihar Dzeravianka <Ihar_Dzeravianka@epam.com>
2022-12-08 12:20:32 +0300

    feat: scenarios

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
 import SwiperCore, { Navigation, Pagination } from 'swiper';
 import { SwiperModule } from 'swiper/angular';
 
 import { HoldButtonModule } from '../../shared/modules/hold-button/hold-button.module';
+import { ActionSelectorComponent } from './components/action-selector/action-selector.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MainControlButtonsComponent } from './components/main-control-buttons/main-control-buttons.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
@@ -17,6 +21,7 @@ SwiperCore.use([Navigation, Pagination]);
   declarations: [
     DashboardComponent,
     MainControlButtonsComponent,
+    ActionSelectorComponent,
   ],
   imports: [
     CommonModule,
@@ -26,6 +31,9 @@ SwiperCore.use([Navigation, Pagination]);
     MatProgressSpinnerModule,
     HoldButtonModule,
     SwiperModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
   ],
 })
 export class DashboardModule { }
