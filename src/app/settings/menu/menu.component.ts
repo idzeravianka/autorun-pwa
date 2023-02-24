0fcd46f9738b9c6fee2c6f476ad5334b15aa016e
Ihar Dzeravianka <Ihar_Dzeravianka@epam.com>
2023-02-24 16:51:00 +0300

    feat: enable edit functionality

--- src/app/settings/menu/menu.component.ts
+++ src/app/settings/menu/menu.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { BehaviorSubject } from 'rxjs';
 
+import { WINDOW_OBJECT } from '../../app.module';
 import { MqttService } from '../../core/services/mqtt.service';
 
 @Component({
@@ -13,9 +14,17 @@ export class MenuComponent {
   public hasInternetConnection$: BehaviorSubject<boolean> = this.mqttService.hasInternetConnection$;
   public version$: BehaviorSubject<string> = this.mqttService.version$;
 
-  constructor(private mqttService: MqttService, private navController: NavController) { }
+  constructor(
+    private mqttService: MqttService,
+    private navController: NavController,
+    @Inject(WINDOW_OBJECT) private window: Window,
+  ) { }
 
   public navigateTo(urlSegment: string): void {
     this.navController.navigateForward(urlSegment);
+  }
+
+  public openTelegram(): void {
+    this.window.open('https://t.me/+Vw4C60B8Yqob_JWc', '_blank');
   }
 }
