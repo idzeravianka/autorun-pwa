e8918b6d3b846ab6208a7ad5a508d7a7b2598466
Nobody Knows <nb6.knws@gmail.com>
2026-04-25 14:51:20 +0300

    update header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { IonApp, IonRouterOutlet, IonHeader, IonContent } from '@ionic/angular/standalone';
+import { AsyncPipe, CommonModule, Location } from '@angular/common';
+import { Component, DestroyRef, inject, OnInit, Signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NavigationEnd, Router, RouterLinkActive } from '@angular/router';
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonHeader,
+  IonContent,
+  IonBackButton,
+  IonIcon,
+  NavController,
+} from '@ionic/angular/standalone';
+import { filter, map, Observable, skip } from 'rxjs';
 
-import { HeaderComponent } from './core/components/header/header.component';
 import { MqttService } from './core/services/mqtt.service';
 
 @Component({
@@ -9,13 +20,47 @@ import { MqttService } from './core/services/mqtt.service';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonApp, IonRouterOutlet, IonHeader, IonContent, HeaderComponent],
+  imports: [
+    IonApp,
+    IonRouterOutlet,
+    IonHeader,
+    IonContent,
+    AsyncPipe,
+    RouterLinkActive,
+    CommonModule,
+    IonBackButton,
+    IonIcon,
+  ],
   host: {
     '(document:visibilitychange)': 'onVisibilityChange()',
   },
 })
 export class AppComponent implements OnInit {
+  public isSettingsOpen$: Observable<boolean>;
+  public isCoreRoute$: Observable<boolean>;
+  public isEditDashboardModeEnabled: Signal<boolean>;
+
+  private navigationHistory: string[] = [];
+
+  private destroyRef = inject(DestroyRef);
+  private router: Router = inject(Router);
+  private location: Location = inject(Location);
+  private navController: NavController = inject(NavController);
   private mqttService: MqttService = inject(MqttService);
+
+  constructor() {
+    this.isSettingsOpen$ = this.router.events.pipe(
+      skip(1),
+      filter((event) => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => event.url.includes('settings')),
+    );
+    this.isCoreRoute$ = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => event.url === '/' || event.url.includes('home')),
+    );
+
+    this.isEditDashboardModeEnabled = this.mqttService.isEditDashboardModeEnabled;
+  }
 
   ngOnInit() {
     // this.newVersionPromptService.checkAppVersion();
@@ -25,11 +70,36 @@ export class AppComponent implements OnInit {
       this.mqttService.connect();
     }
     this.mqttService.setTimerData();
+
+    this.router.events.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((event) => {
+      if (event instanceof NavigationEnd) this.navigationHistory.push(event.urlAfterRedirects);
+    });
   }
 
   public onVisibilityChange(): void {
     if (document.visibilityState === 'visible') {
       this.mqttService.updateSensorsData();
     }
+  }
+
+  public async navigateToSettings(): Promise<void> {
+    await this.navController.navigateForward('/settings');
+  }
+
+  public async navigationBack(): Promise<void> {
+    this.navController.setDirection('back');
+    this.navigationHistory.pop();
+    if (this.navigationHistory.length > 0) {
+      this.location.back();
+    } else {
+      await this.router.navigateByUrl('/');
+    }
+  }
+
+  public switchDashboardEditMode(): void {
+    if (this.isEditDashboardModeEnabled()) {
+      this.mqttService.saveDashboardElementsSettings();
+    }
+    this.mqttService.toggleDashboardEditMode();
   }
 }
